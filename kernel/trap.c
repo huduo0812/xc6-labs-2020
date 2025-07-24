254407a4a2e30ed95f92c9245f97add4639c976b
胡舵 <huduo0713@163.com>
2025-07-24 16:49:06 +0800

    完成lab4 ！🎉

--- kernel/trap.c
+++ kernel/trap.c
@@ -77,8 +77,19 @@ usertrap(void)
     exit(-1);
 
   // give up the CPU if this is a timer interrupt.
-  if(which_dev == 2)
+  // give up the CPU if this is a timer interrupt.
+  if (which_dev == 2) {
+      if (p->alarm_interval != 0 && --p->alarm_ticks <= 0 && p->alarm_goingoff == 0) {
+      	  // 是否设置了时钟 && 时钟倒计时是否结束 && 没有其他时钟正在运行
+          // 如果一个时钟到期的时候已经有一个时钟处理函数正在运行，
+          // 则会推迟到原处理函数运行完成后的下一个 tick 才触发这次时钟
+          p->alarm_ticks = p->alarm_interval;      // 重置时钟倒计时
+          *p->alarm_trapframe = *p->trapframe;          // 保存当前进程陷阱帧
+          p->trapframe->epc = (uint64)p->alarm_handler; // 跳转到时钟回调函数
+          p->alarm_goingoff = 1;                        // 标记当前已有时钟正在运行
+      }
     yield();
+  }
 
   usertrapret();
 }
@@ -218,3 +229,19 @@ devintr()
   }
 }
 
+// 设置进程中时钟的相关属性
+int sigalarm(int ticks, void(*handler)()) {
+    struct proc* p = myproc();
+    p->alarm_interval = ticks;
+    p->alarm_handler = handler;
+    p->alarm_ticks = ticks;
+    return 0;
+}
+
+//将进程恢复到alarm中断前的状态
+int sigreturn() {
+    struct proc* p = myproc();
+    *p->trapframe = *p->alarm_trapframe;
+    p->alarm_goingoff = 0;
+    return 0;
+}
