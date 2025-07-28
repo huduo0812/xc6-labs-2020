8b350abb1ed46e9dcb738c84c588c3af55c03e24
胡舵 <huduo0713@163.com>
2025-07-28 17:07:33 +0800

    完成了lazy alloc后的page fault 的处理。

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,6 +67,19 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  } else if (r_scause() == 13 || r_scause() == 15) { // 惰性分配导致的缺页异常
+    uint64 fault_va = r_stval();      // 获取引发缺页异常的虚拟地址
+      char* pa = 0;                     // 分配的物理地址
+      // 判断fault_va是否在进程堆空间之中
+      if (PGROUNDUP(p->trapframe->sp) - 1 < fault_va && fault_va < p->sz && (pa = kalloc()) != 0) {
+          memset(pa, 0, PGSIZE);
+          // 物理内存映射
+          if (mappages(p->pagetable, PGROUNDDOWN(fault_va), PGSIZE, (uint64)pa, PTE_R | PTE_W | PTE_X | PTE_U) != 0) {      
+              printf("lazy alloc: failed to map page\n");
+              kfree(pa);
+              p->killed = 1;
+          }
+      }
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
