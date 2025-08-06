543d7c89c69ac7112264d1a10329ba512ff1ce43
胡舵 <huduo0713@163.com>
2025-08-06 17:18:45 +0800

    修改了 NDIRECT 和 MAXFILE的大小，

--- kernel/fs.h
+++ kernel/fs.h
@@ -24,9 +24,9 @@ struct superblock {
 
 #define FSMAGIC 0x10203040
 
-#define NDIRECT 12
+#define NDIRECT 11
 #define NINDIRECT (BSIZE / sizeof(uint))
-#define MAXFILE (NDIRECT + NINDIRECT)
+#define MAXFILE (NDIRECT + NINDIRECT + NINDIRECT * NINDIRECT)
 
 // On-disk inode structure
 struct dinode {
